e4a4657c90292ec2a125aec9efd1d73f424a0b00
Carlos Davila <cbdavila@asu.edu>
2016-04-26 21:48:35 -0700

    Country table insertions

---
+++ DataFolder/CountryScan.java
@@ -0,0 +1,63 @@
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.util.Scanner;
+
+public class CountryScan {
+	String countryName = "";
+	String dbName = "";
+	String population = "";
+	String countryID = "";
+	String query = "";
+	String filename = "languages.txt";
+	
+	public CountryScan(){
+		
+	}
+	
+	public void getFileContents(){
+		Scanner scanner;
+		try {
+            Scanner input = new Scanner(System.in);		//reads data
+           // FileWriter fw = new FileWriter(outputFile);
+			//BufferedWriter bw = new BufferedWriter(fw);	//write output
+			
+            System.out.println("Enter your database name");
+            dbName = input.nextLine();
+            
+            System.out.println("You entered : " +dbName);
+            
+            File file = new File(filename);
+            System.out.println("Reading From ... " +filename);
+            //System.out.println("Writing to ..." +outputFile);
+
+            input = new Scanner(file);
+            while(input.hasNextLine()){
+            	query=input.nextLine();
+            	query=query.replace("projectdb", dbName);
+            	System.out.println(query);
+            }
+            
+           /* while (input.hasNextLine()) {
+            	this.countryID = input.next();
+            	this.countryName = input.next();
+            	this.population = input.next();
+            	query = ("INSERT INTO `"+dbName+"`.`Country` (`CountryId`, `CountryName`, `Population`) VALUES ('"+countryID+"', '"+countryName+"', '"+population+"');");
+            	System.out.println(query);
+            }
+            bw.close();
+            */
+            input.close();
+
+        } catch (Exception ex) {
+            System.out.println("End of File");
+        }
+		
+	}
+	
+	public static void main(String args[]){
+		CountryScan test =new CountryScan();
+		test.getFileContents();
+		
+	}
+}
